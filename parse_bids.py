bb9094780e5888f144bd339a18ca5281913821f2
JVP3122 <jeff.pohlmeyer@gmail.com>
2019-02-14 00:55:34 -0500

    Created base route of Vue app to point to /ssdl instead of /. Added v-data-table to display data. Created method to show bids by player.  Addressed todo of checking for validity, though not doing anything with it right now.

--- parse_bids.py
+++ parse_bids.py
@@ -1,6 +1,7 @@
 # todo: add code that checks to see if any bids are invalid
 
 import pandas as pd
+import numpy as np
 import json
 
 
@@ -22,7 +23,18 @@ def generate_winning_bids_file(file_name, df, width=2, precision=4):
             for idx, row in winning_df.iterrows():
                 year_string = 'year' if row.Years == 1 else 'years'
                 f.write(f'{row.Player}, {row.Years} {year_string} at {row.AAV:{width}.{precision}}/year\n')
-            winning_df.set_index('Player', inplace=True)
+            f.write('\n')
+
+
+def generate_all_bids_file(file_name, df, width=2, precision=4):
+    with open(file_name, 'w') as f:
+        for player in df.Player.unique():
+            f.write(f'[b][u]{player}[/u][/b]\n')
+            player_bids = df.loc[df['Player'] == player].sort_values(by=['AAV', 'Years', 'Y1', 'Order'], ascending=[False, False, False, True])
+            for idx, row in player_bids.iterrows():
+                year_string = 'year' if row.Years == 1 else 'years'
+                breakdown_string = f' ({row.Breakdown})' if isinstance(row.Breakdown, str) else ''
+                f.write(f'{row.Team}: {row.Years} {year_string} at {row.AAV:{width}.{precision}}/year{breakdown_string}\n')
             f.write('\n')
 
 
@@ -30,8 +42,8 @@ def generate_total_bids_json(file_name, df):
     winning_bids = {}
     for player in df.Player.unique():
         winning_df = df.loc[df['Player'] == player].drop(['Player', 'Order'], axis=1)
-    winning_df.set_index('Team', inplace=True)
-    winning_bids[player] = winning_df.transpose().to_json()
+        winning_df.set_index('Team', inplace=True)
+        winning_bids[player] = winning_df.transpose().to_json()
 
     with open(file_name, 'w') as fp:
         json.dump(winning_bids, fp)
@@ -40,17 +52,27 @@ def generate_total_bids_json(file_name, df):
 df = pd.read_csv('bids.csv')
 
 df['AAV'] = df['Total'] / df['Years']
-df['Y1'] = pd.to_numeric(df['Breakdown'].str.split(",", n=1, expand=True)[0]).fillna(df['AAV'])
+for i in range(0, 5):
+    val = ''
+    try:
+        df[f'Y{i+1}'] = pd.to_numeric(df['Breakdown'].str.split(",", n=1, expand=True)[i]).fillna(df['AAV'])
+    except Exception as e:
+        df[f'Y{i+1}'] = df['AAV']
 
-teams = df['Team'].unique()
-players = df['Player'].unique()
+df['Max'] = df[['Y1', 'Y2', 'Y3', 'Y4', 'Y5']].values.max(1)
+df['Min'] = df[['Y1', 'Y2', 'Y3', 'Y4', 'Y5']].values.min(1)
+df['MaxValid'] = df['AAV'] * 1.2 > df['Max']
+df['MinValid'] = df['AAV'] * 0.8 < df['Min']
+df['BaseValid'] = df['AAV'] > 0.4
+df['Valid'] = df['MaxValid'] & df['MinValid'] & df['BaseValid']
+
+generate_all_bids_file('player_bids.txt', df)
+generate_total_bids_json('all_bids.json', df)
 
 df = max_by_column(df, 'Player', 'AAV')
 df = max_by_column(df, 'Player', 'Years')
 df = max_by_column(df, 'Player', 'Y1')
 df = min_by_column(df, 'Player', 'Order')
-df.drop('Y1', axis=1, inplace=True)
+df.drop(['Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Max', 'Min'], axis=1, inplace=True)
 
 generate_winning_bids_file('winning_bids.txt', df)
-
-generate_total_bids_json('all_bids.json', df)
